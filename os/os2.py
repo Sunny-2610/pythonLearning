db503b4894a430c1a0c8095ee7c5426ee0762300
Sunny-2610 <sunnysinha9884@gmail.com>
2025-12-31 16:29:37 +0530

    os 2 & 3 are done

---
+++ os/os2.py
@@ -0,0 +1,5 @@
+import os
+
+# List all files and folders in current directory
+files = os.listdir('.')
+print("Files and Folders:", files)
---
+++ os/os3.py
@@ -0,0 +1,9 @@
+import os
+
+# Create a new folder
+folder_name = "my_folder"
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+    print(f"Folder '{folder_name}' created.")
+else:
+    print(f"Folder '{folder_name}' already exists.")
