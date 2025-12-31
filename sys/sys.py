7bc116e1c42cf70604f10c12927211fea6ceddaa
Sunny-2610 <sunnysinha9884@gmail.com>
2025-12-31 16:22:04 +0530

    sys is done

---
+++ sys/sys.py
@@ -0,0 +1,11 @@
+#sys is used to intract with the runtime python environment
+import sys
+
+# Python version
+print(sys.version)
+
+# Command line arguments
+print(sys.argv)
+
+# Exit a program
+# sys.exit("Exiting program")
